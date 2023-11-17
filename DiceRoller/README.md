b7e6ae219f24c6de042dbd74aa4750a1a7e86ce5
SHM4 <ajdwk91@naver.com>
2023-11-17 18:26:13 +0900

    일단 다이스 굴리기 앱 완성

---
+++ DiceRoller/README.md
@@ -0,0 +1,15 @@
+# 이미지 저장 방법
+View > Tool Windows > Resource Manager > + 클릭 > Import Drawables >
+
+# Learn more
+- Jetpack Compose Tutorial : https://developer.android.com/jetpack/compose/tutorial
+- Add Jetpack Compose toolkit dependencies : https://developer.android.com/jetpack/compose/setup#compose-compiler
+- Get started with Jetpack Compose : https://developer.android.com/jetpack/compose/documentation
+- Basics of Composable functions : https://developer.android.com/jetpack/compose/layouts/basics#composable-functions
+- Button composable : https://developer.android.com/reference/kotlin/androidx/compose/material3/package-summary#Button(kotlin.Function0,androidx.compose.ui.Modifier,kotlin.Boolean,androidx.compose.ui.graphics.Shape,androidx.compose.material3.ButtonColors,androidx.compose.material3.ButtonElevation,androidx.compose.foundation.BorderStroke,androidx.compose.foundation.layout.PaddingValues,androidx.compose.foundation.interaction.MutableInteractionSource,kotlin.Function1)
+- Image composable : https://developer.android.com/reference/kotlin/androidx/compose/foundation/package-summary#Image(androidx.compose.ui.graphics.ImageBitmap,kotlin.String,androidx.compose.ui.Modifier,androidx.compose.ui.Alignment,androidx.compose.ui.layout.ContentScale,kotlin.Float,androidx.compose.ui.graphics.ColorFilter,androidx.compose.ui.graphics.FilterQuality)
+- State and Jetpack Compose : https://developer.android.com/jetpack/compose/state
+
+# 다음 진도
+3번부터
+https://developer.android.com/courses/pathways/android-basics-compose-unit-2-pathway-2?index=..%2F..%2Fandroid-training#codelab-https://developer.android.com/codelabs/basic-android-kotlin-compose-build-a-dice-roller-app
--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,24 +3,54 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { // 람다
-            DiceRollerTheme { // 람다
-                DiceRollerApp()
+            DiceRollerTheme { // 이런 게 람다
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DiceRollerApp()
+                }
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()                      // fillMaxSize()를 Modifier 객체에 체이닝. 레이아웃이 전체 화면을 채우도록 함.
+        .wrapContentSize(Alignment.Center)  // wrapContentSize() : 사용 가능한 공간이 최소한 내부에 있는 구성요소만큼 커야 한다고 지정
+        // Alignment 객체 : 레이아웃 내부의 구성요소가 사용 가능한 공간보다 작을 경우, 사용 가능한 공간 내에서 구성요소를 정렬하는 방식을 지정
+    )
 }
 
 @Composable
@@ -30,20 +60,31 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) { // modifier : Modifi
                                                             // 왜냐하면 컴포저블이 재구성(컴포저블 메서드의 코드 블록이 재실행되는 것)을 거칠 수 있기 때문이다.
                                                             // Modifier 객체가 코드 블록에서 만들어지면 다시 만들어질 수 있다? -> 후반부에 더 다룰 예정
                                                             // 그렇기 때문에 DiceRollerApp()에서 Modifier 인수를 전달하는 것
+    var result by remember { // remember : composable의 재구성 시 상태가 유지되도록 한다.
+        mutableStateOf(1) // mutableStateOf : 관찰 가능한 상태를 생성하여 composable이 상태의 변경에 반응할 수 있도록 한다.
+    }
+    val imageResource = when (result) { // when : Java의 switch에 해당하는 표현식
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     Column ( // Column() 함수 : 하위 요소를 세로 순서로 배치하는 컴포저블 레이아웃
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {}
+        modifier = modifier, // 상위 modifier 객체를 준수
+        horizontalAlignment = Alignment.CenterHorizontally // 자식 요소들이 화면 중간에 위치하게 한다.
+    ) {
+        Image(
+            painter = painterResource(id = imageResource),
+            contentDescription = result.toString()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random() }) {
+            Text(stringResource(id = R.string.roll), fontSize = 24.sp)
+        }
+    }
 }
 
 // Modifier 클래스 : 컴포저블을 장식하거나 컴포저블에 동작을 추가한다.
 
-@Preview
-@Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage(modifier = Modifier
-        .fillMaxSize()                      // fillMaxSize()를 Modifier 객체에 체이닝. 레이아웃이 전체 화면을 채우도록 함.
-        .wrapContentSize(Alignment.Center)  // wrapContentSize() : 사용 가능한 공간이 최소한 내부에 있는 구성요소만큼 커야 한다고 지정
-                                            // Alignment 객체 : 레이아웃 내부의 구성요소가 사용 가능한 공간보다 작을 경우, 사용 가능한 공간 내에서 구성요소를 정렬하는 방식을 지정
-    )
-}
